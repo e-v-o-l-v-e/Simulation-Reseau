f0fce93330fa1c44b49004048b0325328e798b00
evolve <elivanoa5@gmail.com>
2025-05-22 17:30:18 +0200

    simple main, add makefile

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include "network.h"
+#include <stdio.h>
+#define LINE_LENGHT 64
 
 int main() {
   network n; 
+
+  FILE *config;
+  config = fopen("config1", "r");
+
+  if (config == NULL) {
+    perror("le fichier de configuration n'existe pas ou n'est pas lisible.");
+    return 0;
+  }
+
+  char line[LINE_LENGHT];
+
+  char *header = fgets(line, LINE_LENGHT, config);
+
+  printf("%s", header);
 }
